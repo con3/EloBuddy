b1b05b040e610fa0eaf05fe06b9069d42341e715
Lucas Machado <lucasllaw@gmail.com>
2015-10-22 15:14:04 -0200

    Starting ashe

--- Con3AIO/Program.cs
+++ Con3AIO/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
 
 namespace Con3AIO
 {
@@ -10,6 +10,39 @@ namespace Con3AIO
     {
         static void Main(string[] args)
         {
+            //Espera até que o jogo seja carregado
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            //Verifica qual campeão o jogador está usando
+            switch(Player.Instance.ChampionName)
+            {
+                case "Alistar":     //TODO
+                case "Amumu":       //TODO
+                case "Annie":       //TODO
+                case "Ashe":        //TODO
+                case "Dr. Mundo":   //TODO
+                case "Garen":       //TODO
+                case "Jax":         //TODO
+                case "Kayle":       //TODO
+                case "Master Yi":   //TODO
+                case "Miss Fortune"://TODO
+                case "Nunu":        //TODO
+                case "Ryze":        //TODO
+                case "Sivir":       //TODO
+                case "Tristana":    //TODO
+                case "Volibear":    //TODO
+                case "Warwick":     //TODO
+                default:
+                    return;
+            }
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+
         }
     }
 }
